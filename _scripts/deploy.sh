d1a83e62725ef1a920715b2a83bea9bdc01df33a
Shander Lam <shanderlam@gmail.com>
2012-04-30 02:03:23 +0800

    Modify deploy script.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env sh
-rsync -arvzP --delete --exclude-from=../.gitignore --delete-excluded ../_site/ root@fs.shanderlam.com:/home/www/blog.shanderlam.com/
+rsync -arvzP --delete --delete-excluded ../_site/ root@fs.shanderlam.com:/home/www/blog.shanderlam.com/
