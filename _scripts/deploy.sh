df469071bfff12bc05dc81c8a500223fef59a06e
Shander Lam <shanderlam@gmail.com>
2012-09-16 17:53:28 +0800

    Use www account for deploying site.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env sh
-rsync -arvzP --delete --delete-excluded _site/ root@fs.shanderlam.com:/home/www/blog.shanderlam.com/
+rsync -arvzP --delete --delete-excluded _site/ www@fs.shanderlam.com:/home/www/blog.shanderlam.com/
